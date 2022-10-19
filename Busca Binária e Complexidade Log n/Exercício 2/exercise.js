b1244e2ecb6d6138ae2708901b213c3ec269c32d
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-10-19 09:43:45 -0300

    add exercise 2

--- Busca Binária e Complexidade Log n/Exercício 2/exercise.js
+++ Busca Binária e Complexidade Log n/Exercício 2/exercise.js
@@ -5,24 +5,25 @@ function findFirstIndex(lst, target) {
     const middle = Math.floor((left + right) / 2);
     const potentialMatch = lst[middle];
     if (target === potentialMatch) {
-      let before = middle - 1;
-      if (target === lst[before]) {
-        while (before >= 0 && target === lst[before]) {
-          before--;
-        }
-        console.log(before + 1);
-        return before + 1;
+      right = middle;
+      while (left <= right) {
+        const meio = Math.floor((left + right) / 2);
+        const potentialMatch2 = lst[meio];
+        if (left === right) left = right + 1;
+        if (target === potentialMatch2) right = meio;
+        if (target > potentialMatch2) left = meio + 1;
       }
-      lst[middle + 1] === target ? console.log(middle) : console.log(-1);
-      return lst[middle + 1] === target ? middle : -1;
+      console.log(right)
+      return right;
     }
     if (target < potentialMatch) right = middle - 1;
     if (target > potentialMatch) left = middle + 1;
   }
-  return -1;
+  console.log(-1)
+  return -1
 }
 
-findFirstIndex([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11, 12, 13, 14, 15], 2); // 1
+findFirstIndex([2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11, 12, 13, 14, 15], 2); // 0
 findFirstIndex([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11, 12, 13, 14, 15], 10); // 9
-findFirstIndex([1, 2, 2, 3, 4, 4, 5, 6, 6, 5, 7, 7], 7); // 10
-findFirstIndex([1, 2, 2, 3, 4, 4, 5, 6, 6, 5, 7, 7], 8); // -1
+findFirstIndex([1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7], 6); // 8
+findFirstIndex([1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7], 8); // -1
