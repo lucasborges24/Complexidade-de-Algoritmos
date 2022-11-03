f8d461123c20d6a2b90a72181dc71428e2f422e7
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-11-03 15:48:38 -0300

    second attempt

--- Busca Binária e Complexidade Log n/Exercício 4/exercise.js
+++ Busca Binária e Complexidade Log n/Exercício 4/exercise.js
@@ -1,6 +1,7 @@
 function countWord(wordList, word) {
   let count = 0;
-  
+
+  let lastRight = Infinity;
   let left = 0;
   let right = wordList.length - 1;
   while (left <= right) {
@@ -8,11 +9,31 @@ function countWord(wordList, word) {
     const potentialMatch = wordList[middle];
     if (potentialMatch === word) {
       count++;
-      break;
-      //   left = right + 1;
-    } else if (potentialMatch < word) {
+      let i = middle + 1;
+      if (lastRight !== Infinity) {
+        count += lastRight - middle - 1;
+      } else {
+        let left2 = middle;
+        let right2 = right;
+        while (wordList[left2] === word) {
+          const middle2 = Math.floor((left2 + right2) / 2);
+          if (wordList[middle2 + 1] !== word) {
+            count += left2 - middle;
+          }
+          if (wordList[middle2] !== word) {
+            right2 = middle2 - 1;
+          } else if (wordList[middle2] === word) {
+            left2 = middle2 + 1;
+          }
+        }
+      }
+      lastRight = middle;
       right = middle - 1;
-    } else {
+    }
+    if (potentialMatch > word) {
+      right = middle - 1;
+    }
+    if (potentialMatch < word) {
       left = middle + 1;
     }
   }
@@ -21,14 +42,28 @@ function countWord(wordList, word) {
 }
 
 countWord(
-  ["abelha", "alvo", "alvo", "árvore", "colméia", "colméia", "colméia", "da"],
-  "árvore"
+  ["abelha", "alvo", "alvo", "arvore", "colméia", "colméia", "colméia", "da"],
+  "arvore"
 ); // 1
 countWord(
-  ["abelha", "alvo", "alvo", "árvore", "colméia", "colméia", "colméia", "da"],
+  [
+    "abelha",
+    "alvo",
+    "alvo",
+    "arvore",
+    "colméia",
+    "colméia",
+    "colméia",
+    "da",
+    "da",
+    "de",
+    "de",
+    "de",
+    "do",
+  ],
   "colméia"
 ); // 3
 countWord(
-  ["abelha", "alvo", "alvo", "árvore", "colméia", "colméia", "colméia", "da"],
+  ["abelha", "alvo", "alvo", "arvore", "colméia", "colméia", "colméia", "da"],
   "balde"
 ); // 0
